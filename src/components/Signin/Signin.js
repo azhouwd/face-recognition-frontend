d168a33a37c7d63f7f08916867f4501ac4369c54
Ang Zhou <dumplingman@Angs-MacBook-Pro.local>
2019-01-14 19:24:09 -0500

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/radiant-headland-43699.git/signin', {
+    fetch('https://radiant-headland-43699.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
